964d765c909171463463c1464f7c1e1e14e5787c
Suyash Jain <suyashj@uci.edu>
2024-06-22 20:48:12 +0530

    renamed creds to mail_send

--- creadentials.py
+++
@@ -1,9 +0,0 @@
-# needs a gmail account 
-# requirements email and pass word set up via https://support.google.com/accounts/answer/185833
-# using this method get a app password so you dont need verification to 2FA while using it
-# create a custom password
-
-
-sender_email = "xyz@gmail.com" # email via which email gets sent
-sender_password = "vksy giop ssry xlxz" # pasword set via app password on site I gave above
-recipient_email = "xyz@gmail.com"# you can use the same mail to prevent spam detection
