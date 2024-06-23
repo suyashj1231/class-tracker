4fb3583da97fd79c04d1efacb5568754bd35cf55
Suyash Jain <suyashj@uci.edu>
2024-06-23 05:47:06 +0530

    added send email fuunction

--- mail_send.py
+++ mail_send.py
@@ -8,18 +8,25 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 sender_email = "xyz@gmail.com" # email via which email gets sent
-sender_password = "vksy giop ssry xlxz" # pasword set via app password on site I gave above
+sender_password = "xxxx yyyy zzzz wwww" # pasword set via app password on site I gave above
 recipient_email = "xyz@gmail.com"# you can use the same mail to prevent spam detection
 
-# sample URL
 
-# to get url for your site go to websoc -> search for your class via code, number etc 
-# -> att the bottom of page click -> add bookmark -> now copt that URL here.
-url = 'https://www.reg.uci.edu/perl/WebSoc?YearTerm=2024-92&ShowFinals=1&ShowComments=1&Dept=COGS&CourseNum=108'
-
-header = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-} # to prevent bot behavior
-
-
+def send_email(subject, body, sender_email, sender_password, recipient_email):
+    try:
+        print("Sending email...")
+        msg = MIMEMultipart()
+        msg['From'] = sender_email
+        msg['To'] = recipient_email
+        msg['Subject'] = subject
+        msg.attach(MIMEText(body, 'plain'))
+        
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(sender_email, sender_password)
+        server.send_message(msg)
+        server.quit()
+        print("Email notification sent successfully.")
+    except Exception as e:
+        print(f"An error occurred while sending the email: {str(e)}")
 
---
+++ tracker.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import winsound
+
+
+# sample URL
+# to get url for your site go to websoc -> search for your class via code, number etc 
+# -> att the bottom of page click -> add bookmark -> now copt that URL here.
+url = 'https://www.reg.uci.edu/perl/WebSoc?YearTerm=2024-92&ShowFinals=1&ShowComments=1&Dept=COGS&CourseNum=108'
+
+header = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+} # to prevent bot behavior
+
+def check_enrollment():
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    rows = soup.find_all('tr')
+    
+    enrollments = {}
+    for row in rows[1:]:  # Skip the header row
+        columns = row.find_all('td')
+        if len(columns) >= 12:
+            code = columns[0].text.strip()
+            enroll_count = int(columns[10].text.strip())  # Get the current enrollment count (Enr)
+            enrollments[code] = enroll_count
+    
+    return enrollments
+
